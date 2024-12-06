c3d42407196d99949ac24d6cbb5ca5a1d6af9012
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2024-12-06 11:45:28 -0300

    Iniciando projeto

---
+++ src/br/com/ehmf/academico_appsimple/Academico_AppSimple.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package br.com.ehmf.academico_appsimple;
+
+/**
+ *
+ * @author duduh
+ */
+public class Academico_AppSimple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Iniciando aplicação");
+    }
+    
+}
